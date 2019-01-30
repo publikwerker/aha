06b249ae157cda79b0e47587cabb71c051636cfb
Publikwerker <jason.michael.hoffman@gmail.com>
2019-01-30 06:01:43 -0800

    Updates

---
+++ precache-manifest.4d05a2ee466b7324c88b6ed93785bb97.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1bab74ad604e402308be",
+    "url": "/aha/static/css/main.0d1e90db.chunk.css"
+  },
+  {
+    "revision": "1bab74ad604e402308be",
+    "url": "/aha/static/js/main.1bab74ad.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/aha/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "969afec2c1259e120a4e",
+    "url": "/aha/static/js/runtime~main.969afec2.js"
+  },
+  {
+    "revision": "7cd95ce015e1cdb4a0427de327494464",
+    "url": "/aha/index.html"
+  }
+];
